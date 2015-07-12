5feb52af962f4f03391a4e241d0df3a66b8e19de
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-07-12 02:07:23 +0100

    Adding copyright notices

--- mc-dropbox-server/mc-dropbox-server.py
+++ mc-dropbox-server/mc-dropbox-server.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3
+##
+## Copyright (C) 2015 João Ricardo Lourenço <jorl17.8@gmail.com>
+##
+## Github: https://github.com/Jorl17
+##
+## Project main repository: https://github.com/Jorl17/minecraft-dropbox-server
+##
+## This file is part of minecraft-dropbox-server.
+##
+## minecraft-dropbox-server is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## minecraft-dropbox-server is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with minecraft-dropbox-server.  If not, see <http://www.gnu.org/licenses/>.
+##
 from optparse import OptionParser
 import os
 import subprocess
